e04f9a733d661a19df1b1d9939681e9b2a00470e
Wisani Shilumani <wshilumani@gmail.com>
2026-01-04 09:23:45 +0200

    fix: dont mutate console

--- services/polymarket/index.ts
+++ services/polymarket/index.ts
@@ -2,25 +2,25 @@ import { ClobClient } from '@polymarket/clob-client';
 import { Wallet } from '@ethersproject/wallet';
 import { POLYMARKET_CLOB_URL, POLYMARKET_FUNDER, POLYMARKET_PRIVATE_KEY } from '../../config';
 
-const muteConsole: any = {
-  log: () => {},
-  error: () => {},
-  warn: () => {},
-  info: () => {},
-  debug: () => {},
-};
+// const muteConsole: any = {
+//   log: () => {},
+//   error: () => {},
+//   warn: () => {},
+//   info: () => {},
+//   debug: () => {},
+// };
 /**
  * Gets an initialized CLOB client for trading
  */
 export const getClobClient = async (): Promise<ClobClient> => {
-  const originalConsole = console;
+  // const originalConsole = console;
   const host = POLYMARKET_CLOB_URL;
   const funder = POLYMARKET_FUNDER;
   const signer = new Wallet(POLYMARKET_PRIVATE_KEY);
   const signatureType = 1;
-  console = muteConsole;
+  // console = muteConsole;
   const creds = new ClobClient(host, 137, signer).createOrDeriveApiKey();
   const client = new ClobClient(host, 137, signer, await creds, signatureType, funder);
-  console = originalConsole;
+  // console = originalConsole;
   return client;
 };
