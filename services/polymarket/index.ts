0d4e78d301d136cb76a9440c149fb97437ec3608
Wisani Shilumani <wshilumani@gmail.com>
2026-01-04 10:54:23 +0200

    fix: rm noisy logs

--- services/polymarket/index.ts
+++ services/polymarket/index.ts
@@ -13,11 +13,17 @@ import { POLYMARKET_CLOB_URL, POLYMARKET_FUNDER, POLYMARKET_PRIVATE_KEY } from '
  * Gets an initialized CLOB client for trading
  */
 export const getClobClient = async (): Promise<ClobClient> => {
+  const originalConsoleError = console.error;
   const host = POLYMARKET_CLOB_URL;
   const funder = POLYMARKET_FUNDER;
   const signer = new Wallet(POLYMARKET_PRIVATE_KEY);
   const signatureType = 1;
+  console.error = (...args: any) => {
+    if (args.some((arg: any) => typeof arg === 'string' && arg.includes('Could not create api key'))) return;
+    originalConsoleError(...args);
+  };
   const creds = new ClobClient(host, 137, signer).createOrDeriveApiKey();
   const client = new ClobClient(host, 137, signer, await creds, signatureType, funder);
+  console.error = originalConsoleError;
   return client;
 };
