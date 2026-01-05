b4c8343b7a68b52611846d32154edc4d0be33a36
Wisani Shilumani <wshilumani@gmail.com>
2026-01-05 19:51:56 +0200

    fix: balance cache

--- services/polymarket/account-balance/index.ts
+++ services/polymarket/account-balance/index.ts
@@ -1,14 +1,6 @@
 import { AssetType } from '@polymarket/clob-client';
 import { getClobClient } from '..';
 import logger from '../../../utils/logger';
-import { LRUCache } from 'lru-cache';
-import { POLYMARKET_FUNDER } from '../../../config';
-
-const balanceCache = new LRUCache<string, number>({
-  max: 1,
-  ttl: 100,
-  allowStale: false,
-});
 
 /**
  * Gets the account balance from Polymarket
@@ -16,12 +8,9 @@ const balanceCache = new LRUCache<string, number>({
  */
 export const getAccountCollateralBalance = async (): Promise<number> => {
   try {
-    const cachedBalance = balanceCache.get(POLYMARKET_FUNDER);
-    if (cachedBalance) return cachedBalance;
     const clobClient = await getClobClient();
     const balances = await clobClient.getBalanceAllowance({ asset_type: AssetType.COLLATERAL });
     const collateralBalance = +(Number(balances.balance) / 1000000).toFixed(3);
-    balanceCache.set(POLYMARKET_FUNDER, collateralBalance);
     return collateralBalance;
   } catch (error) {
     logger.error('Error fetching account balance:', error);
