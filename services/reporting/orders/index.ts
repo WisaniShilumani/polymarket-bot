faa7de9ba7eda728f93efee50150efe739a5d13c
Wisani Shilumani <wshilumani@gmail.com>
2026-01-14 12:32:57 +0200

    none: show all orders

--- services/reporting/orders/index.ts
+++ services/reporting/orders/index.ts
@@ -4,20 +4,25 @@ import { differenceInHours } from 'date-fns';
 import { getMarketByAssetId } from '../../polymarket/markets';
 import { getOutcomePrice } from '../../../utils/prices';
 import { MarketSide } from '../../../common/enums';
-import { formatCurrency } from '../../../utils/accounting';
 
 export const getOrdersReport = async () => {
   const orders = await getOpenOrders();
   const promises = orders.map(async (order) => {
     const market = await getMarketByAssetId(order.market);
-    const priceDifference = Math.abs(Number(order.price) - getOutcomePrice(market, MarketSide.Yes, false));
+    const priceDifference = +(Number(order.price) - getOutcomePrice(market, MarketSide.Yes, false)).toFixed(2);
     const orderAge = differenceInHours(new Date(), new Date(order.created_at * 1000));
     return {
       question: market.question,
       orderAge,
       priceDifference,
+      side: order.side,
     };
   });
   const ordersReport = await Promise.all(promises);
-  return ordersReport;
+  const buyOrdersReport = ordersReport.filter((o) => o.side === Side.BUY);
+  const sellOrdersReport = ordersReport.filter((o) => o.side === Side.SELL);
+  return {
+    buyOrdersReport,
+    sellOrdersReport,
+  };
 };
