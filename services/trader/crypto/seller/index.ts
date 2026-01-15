ea60ad0e49646f25e20a50ad65e6793cfc469a1e
Wisani Shilumani <wshilumani@gmail.com>
2026-01-15 16:48:15 +0200

    fix: efficient seller

--- services/trader/crypto/seller/index.ts
+++ services/trader/crypto/seller/index.ts
@@ -1,46 +1,25 @@
 import { createOrder, getOpenOrders } from '../../../polymarket/orders';
-import type { UserPosition } from '../../../../common/types';
 import { getUserPositions } from '../../../polymarket/positions';
 import { Side } from '@polymarket/clob-client';
-import { getEvent } from '../../../polymarket/events';
+import { getMarketByAssetId } from '../../../polymarket/markets';
 
 export const sellCryptoPositions = async () => {
   const [positions, orders] = await Promise.all([getUserPositions(), getOpenOrders()]);
-  const openOrderMarketIds = new Set(orders.filter((o) => o.side === Side.SELL).map((o) => o.market));
-  const positionsByEventIdMap = new Map<string, UserPosition[]>();
-  positions.forEach((position) => {
-    positionsByEventIdMap.set(position.eventId, [...(positionsByEventIdMap.get(position.eventId) || []), position]);
-  });
 
-  const eventIds = Array.from(positionsByEventIdMap.keys());
-  const eventsPositionsToSell = eventIds.map((eventId) => {
-    const positions = positionsByEventIdMap.get(eventId);
-    if (!positions) return null;
-    return { eventId, positions };
-  });
-
-  for (const event of eventsPositionsToSell) {
-    if (!event?.positions) continue;
-    const marketOrders = event.positions
-      .filter((p) => !openOrderMarketIds.has(p.conditionId))
-      .map((position) => ({
-        tokenId: position.asset,
-        price: position.avgPrice + 0.008, // smaller but faster
-        size: position.size,
-        side: 1,
-      }));
-
-    const eventData = await getEvent(event.eventId);
-    const isCryptoEvent = eventData.tags?.some((t) => t.slug === 'crypto-prices');
+  for (const position of positions) {
+    const market = await getMarketByAssetId(position.conditionId);
+    const isCryptoEvent = market.tags?.includes('Crypto');
     if (!isCryptoEvent) continue;
-    for (const order of marketOrders) {
-      console.log(`Selling ${order.size} shares for ${eventData.title} at ${order.price}`);
-      await createOrder({
-        tokenId: order.tokenId,
-        price: order.price,
-        size: order.size,
-        side: Side.SELL,
-      });
-    }
+    const relatedOrders = orders.filter((o) => o.market === position.conditionId && o.side === Side.SELL);
+    const totalOpenOrderSize = relatedOrders.reduce((acc, o) => acc + Number(o.original_size) - Number(o.size_matched), 0);
+    const remainingSizeToSell = Number(position.size) - totalOpenOrderSize;
+    if (remainingSizeToSell <= 0) continue;
+    console.log(`Selling ${remainingSizeToSell} shares for ${market.question} at ${position.avgPrice + 0.008}`);
+    await createOrder({
+      tokenId: position.asset,
+      price: position.avgPrice + 0.008,
+      size: remainingSizeToSell,
+      side: Side.SELL,
+    });
   }
 };
