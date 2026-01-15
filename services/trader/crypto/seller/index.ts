14a7589c5a637c22d69768262e94af7f4faf7258
Wisani Shilumani <wshilumani@gmail.com>
2026-01-15 08:01:03 +0200

    fix: no strategy

--- services/trader/crypto/seller/index.ts
+++ services/trader/crypto/seller/index.ts
@@ -25,7 +25,7 @@ export const sellCryptoPositions = async () => {
       .filter((p) => !openOrderMarketIds.has(p.conditionId))
       .map((position) => ({
         tokenId: position.asset,
-        price: position.avgPrice + 0.009,
+        price: position.avgPrice + 0.01,
         size: position.size,
         side: 1,
       }));
